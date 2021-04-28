67b3f1e8e87bd810e0c527c4a1d20909432d369e
Joseph Toups <josephtoups95@gmail.com>
2021-04-27 21:18:15 -0700

    defining app

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,30 @@
 const fs = require('fs');
 const {v4:uuidv4} = require('uuid');
 
-
-app.get('./notes.html', (req, res,) => {
-    console.log('getting notes...');
-
-    let data = JSON.parse(fs.readFileSync('./db/dbjsaon', 'utf-8'));
-
-    console.log ('returning notes' + JSON.stringify(data));
-
-    response.json(data);
-});
-
-app.post('./public/notes.html', (req, res) => {
-    const newNote = request.body;
-
-    console.log('Post request' + JSON.stringify(newNote));
-    newNote.id = uuidv4();
+module.exports = function (app){
+    app.get('./notes.html', (req, res,) => {
+        console.log('getting notes...');
     
-    let data = JSON.parse(fs.readFileSync('/db.json', 'utf-8'));
+        let data = JSON.parse(fs.readFileSync('./db/dbjsaon', 'utf-8'));
+    
+        console.log ('returning notes' + JSON.stringify(data));
+    
+        response.json(data);
+    });
+    
+    app.post('./public/notes.html', (req, res) => {
+        const newNote = request.body;
+    
+        console.log('Post request' + JSON.stringify(newNote));
+        newNote.id = uuidv4();
+        
+        let data = JSON.parse(fs.readFileSync('/db.json', 'utf-8'));
+    
+        fs.writeFileSync('/db.jason', JSON.stringify(data));
+    
+        console.log('New note added');
+    
+        response.json(data);
+    });
+}
 
-    fs.writeFileSync('/db.jason', JSON.stringify(data));
-
-    console.log('New note added');
-
-    response.json(data);
-
-});
-
-
-module.exports = app;
