efd573bb91344bc5ee61d3dfb8ff4e28196e6021
Joseph Toups <josephtoups95@gmail.com>
2021-05-02 09:12:11 -0700

    adding delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const fs = require('fs');
 const {v4:uuidv4} = require('uuid');
 const router = require('express').Router();
@@ -30,5 +31,20 @@ router.post('./public/notes', (req, res) => {
     res.json(data);
 });
 
+router.delete('./public/notes', (req, res) => {
+    let noteId = req.params.id.toString();
+
+    console.log(`You\'re DELETING ${noteId}`);
+
+    let data = JSON.parse(fs.readFileSync('./db/bd.json', 'utf-8'));
+
+    const freshData = data.filter(note => note.id.toString() !==noteId);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(freshData));
+
+    console.log(`${noteId} has been deleted`);
+
+    res.json(freshData);
+});
 
 module.exports = router
